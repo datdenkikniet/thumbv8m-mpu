5b3267c569d5f7aeb6f075c04401793d751a78d4
datdenkikniet <jcdra1@gmail.com>
2026-05-30 22:22:00 +0200

    add more useful tests to basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,16 +3,13 @@
 
 use arbitrary_int::u3;
 use cortex_m_rt as _;
+use defmt::unwrap;
 use defmt_rtt as _;
+use panic_probe as _;
 use thumbv8m_mpu::{
     AccessPermissions, AttributeIndex, MemoryAttributes, Mpu, NormalMemoryAttributes, Region,
-    RegionAligned, RegionConfig, Shareability, TransientAllocations,
+    RegionAligned, RegionConfig, RegionRange, Shareability, TransientAllocations,
 };
-
-#[panic_handler]
-fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -21,12 +18,14 @@ fn main() -> ! {
     let dynamic_dma_memory: RegionAligned<[u8; 1022], 2> = RegionAligned::new([0u8; _]);
     let peripherals = cortex_m::Peripherals::take().unwrap();
     let mut mpu = Mpu::new(peripherals.MPU);
-    let mut tokens: [_; 8] = mpu.tokens();
+    let tokens: [_; 8] = mpu.tokens();
 
-    let non_cacheable_index: AttributeIndex = u3::try_new(0).unwrap().into();
+    let [mut one, mut two, ..] = tokens;
+
+    let non_cacheable_index: AttributeIndex = unwrap!(u3::try_new(0).ok()).into();
     mpu.set_attributes(non_cacheable_index, MemoryAttributes::non_cacheable());
 
-    let normal_index: AttributeIndex = u3::try_new(1).unwrap().into();
+    let normal_index: AttributeIndex = unwrap!(u3::try_new(1).ok()).into();
     mpu.set_attributes(
         normal_index,
         MemoryAttributes::Normal {
@@ -54,14 +53,31 @@ fn main() -> ! {
         execute_never: true,
     };
 
-    mpu.set_region(&mut tokens[0], &Region::Enabled(config_static))
-        .unwrap();
-    mpu.set_region(&mut tokens[1], &Region::Enabled(config_dynamic))
-        .unwrap();
+    let mut overlapping_static_config = config_static.clone();
+    let range = config_static.range.get();
+    overlapping_static_config.range = unwrap!(RegionRange::new_inclusive(
+        *range.start() + 32..=*range.end()
+    ));
+
+    unwrap!(mpu.set_region(&mut one, &Region::Enabled(config_static.clone())));
+    // Setting the same region twice is OK
+    unwrap!(mpu.set_region(&mut one, &Region::Enabled(config_static)));
+
+    unwrap!(mpu.set_region(&mut two, &Region::Enabled(config_dynamic)));
+
+    // Creating overlapping, enabled regions is not. It also does
+    // not change the actual value of the region configuration.
+    defmt::println!(
+        "Result of trying to set overlapping region: {}",
+        mpu.set_region(&mut two, &Region::Enabled(overlapping_static_config))
+            .unwrap_err()
+    );
 
     mpu.enable(true, false);
 
     defmt::println!("{}", mpu);
 
-    loop {}
+    loop {
+        cortex_m_semihosting::debug::exit(cortex_m_semihosting::debug::EXIT_SUCCESS);
+    }
 }
